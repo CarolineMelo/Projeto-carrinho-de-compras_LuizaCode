c83f29212d83139ff2dd6deaf1185135092ae7b5
Caroline <2015302@aluno.univesp.br>
2022-09-18 22:59:13 -0300

    crud de produto e carrinho

--- projeto1.py
+++ projeto1.py
@@ -128,9 +128,8 @@ async def criar_endereco(endereco: Endereco, id_usuario: int):
         return OK
     return FALHA
 
+
 # Essa função serve para mostrar os endereços do usuário se o mesmo não possuir endereços vinculado retornará uma lista vazia
-
-
 @app.get("/usuario/{id_usuario}/endereços/")
 async def retornar_enderecos_do_usuario(id_usuario: int):
     if id_usuario in db_usuarios:
@@ -139,7 +138,6 @@ async def retornar_enderecos_do_usuario(id_usuario: int):
 
 
 # Essa função serve para deletar o endereço,porém preservando o usuário.
-
 @app.delete("/endereco/{id_endereco}/")
 async def deletar_endereco(id_endereco: int):
     if id_endereco not in db_end:
@@ -162,6 +160,97 @@ async def retornar_emails(dominio: str):
             emails.append(usuario.email)
     if emails:
         return emails
+    return FALHA
+
+
+# Essa função serve para cadastrar produto, garantindo que produtos distintos não sejam cadastrados com o mesmo id.
+@app.post("/produto/")
+async def criar_produto(produto: Produto):
+    if produto.id in db_produtos:
+        return FALHA
+    db_produtos[produto.id] = produto
+    return OK
+
+
+# Retorna todos os produtos
+@app.get("/produto")
+async def produtos_cadastrados():
+    return db_produtos
+
+
+# Essa função é responsável por deleta produto correspondente ao id_produto, desvinculando-o dos carrinhos dos usuários.
+@app.delete("/produto/{id_produto}/")
+async def deletar_produto(id_produto: int):
+    if id_produto in db_produtos:
+        db_produtos.pop(id_produto)
+        for (key, value) in db_carrinhos.items():
+            # Calculando qual o valor deve ser subtraído do total do carrinho.
+            precos = list(map(lambda prod: prod.preco if prod.id ==
+                          id_produto else None, value['id_produtos']))
+            remover_total = sum(list(filter(None, precos)))
+
+            # Removendo o preço dos produtos excluídos do total do carrinho.
+            db_carrinhos[key]['preco_total'] -= remover_total
+
+            # Deixando a lista somente com os elementos que não foram excluídos.
+            db_carrinhos[key]['id_produtos'] = list(
+                filter(lambda prod: prod.id != id_produto, value['id_produtos']))
+
+            # Alterando a quantidade de produtos do carrinho.
+            db_carrinhos[key]['quantidade_de_produtos'] = len(
+                db_carrinhos[key]['id_produtos'])
+        return OK
+    return FALHA
+
+
+# Essa função é responsável por criar um carrinho de compras e adicionar produto ou apenas adicionar o produto caso o usuário já tenha um carrinho.
+@app.post("/carrinho/{id_usuario}/{id_produto}/")
+async def adicionar_carrinho(id_usuario: int, id_produto: int):
+    if id_produto in db_produtos and id_usuario in db_usuarios:
+        produto = db_produtos[id_produto]
+
+        if db_carrinhos.get(id_usuario, False):
+            # Adiciona produto na lista de produtos
+            db_carrinhos[id_usuario]['id_produtos'].append(produto)
+            # Adiciona o preço do novo produto ao total
+            db_carrinhos[id_usuario]['preco_total'] += produto.preco
+            # Incrementa a quantidade de produtos
+            db_carrinhos[id_usuario]['quantidade_de_produtos'] += 1
+        else:
+            db_carrinhos[id_usuario] = {
+                "id_usuario": id_usuario,
+                "id_produtos": [produto],
+                "preco_total": produto.preco,
+                "quantidade_de_produtos": 1
+            }
+        return OK
+    return FALHA
+
+
+# Essa função é responsável por mostrar todos os itens que ha em um determinado carrinho.
+@app.get("/carrinho/{id_usuario}/")
+async def retornar_carrinho(id_usuario: int):
+    if id_usuario in db_carrinhos:
+        return db_carrinhos[id_usuario]
+    return FALHA
+
+
+# Essa função é responsável por retorna o número de itens e o valor total do carrinho de compras.
+@app.get("/carrinho/{id_usuario}/total")
+async def retornar_total_carrinho(id_usuario: int):
+    if id_usuario in db_carrinhos:
+        numero_itens, valor_total = db_carrinhos[id_usuario][
+            'quantidade_de_produtos'], db_carrinhos[id_usuario]['preco_total']
+        return {"numero_itens": numero_itens, "valor_total": valor_total}
+    return FALHA
+
+
+@app.delete("/carrinho/{id_usuario}/")
+async def deletar_carrinho(id_usuario: int):
+    if id_usuario in db_usuarios:
+        if id_usuario in db_carrinhos:
+            db_carrinhos.pop(id_usuario)
+        return OK
     return FALHA
 
 
