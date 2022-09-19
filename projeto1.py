1a13b84b62137998fedec086b581ac1251fee3f4
Caroline <2015302@aluno.univesp.br>
2022-09-18 22:39:48 -0300

    crud endereço e emails de usuários

--- projeto1.py
+++ projeto1.py
@@ -109,6 +109,60 @@ async def deletar_usuario(id: int):
         return OK
     return FALHA
 
+# Essa função cria um endereço vinculando-o ao usuário.
+
+
+@app.post("/endereco/{id_usuario}/")
+async def criar_endereco(endereco: Endereco, id_usuario: int):
+    if id_usuario in db_usuarios:
+        # Verificando se esse id já pertence ha um endereço cadastrado
+        if endereco.id in db_end:
+            return FALHA
+        db_end[endereco.id] = endereco
+
+        # Verificando se o usuário já possui algum endereço cadastrado
+        if db_end_usr.get(id_usuario, False):
+            db_end_usr[id_usuario].append(endereco)
+        else:
+            db_end_usr[id_usuario] = [endereco]
+        return OK
+    return FALHA
+
+# Essa função serve para mostrar os endereços do usuário se o mesmo não possuir endereços vinculado retornará uma lista vazia
+
+
+@app.get("/usuario/{id_usuario}/endereços/")
+async def retornar_enderecos_do_usuario(id_usuario: int):
+    if id_usuario in db_usuarios:
+        return db_end_usr.get(id_usuario, [])
+    return FALHA
+
+
+# Essa função serve para deletar o endereço,porém preservando o usuário.
+
+@app.delete("/endereco/{id_endereco}/")
+async def deletar_endereco(id_endereco: int):
+    if id_endereco not in db_end:
+        return FALHA
+    # Removendo endereço da lista de endereços.
+    db_end.pop(id_endereco)
+    # Desvinculando endereço removido ao usuário.
+    for (key, value) in db_end_usr.items():
+        db_end_usr[key] = list(
+            filter(lambda end: end.id != id_endereco, value))
+    return OK
+
+
+# Essa função serve para identificar todos os emails que possuírem o mesmo domínio.
+@app.get("/usuarios/emails/")
+async def retornar_emails(dominio: str):
+    emails = []
+    for usuario in db_usuarios.values():
+        if dominio in usuario.email:
+            emails.append(usuario.email)
+    if emails:
+        return emails
+    return FALHA
 
 
 @app.get("/")
