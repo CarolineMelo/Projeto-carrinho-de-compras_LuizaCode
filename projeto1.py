60e0c41307be7d1eafa6ffd495756b2ea78271fe
Caroline <2015302@aluno.univesp.br>
2022-09-18 22:16:57 -0300

    crud usuário

--- projeto1.py
+++ projeto1.py
@@ -11,6 +11,8 @@ OK = "OK"
 FALHA = "FALHA"
 
 # Classe representando os dados do endereço do cliente
+
+
 class Endereco(BaseModel):
     id: int
     rua: str
@@ -19,6 +21,8 @@ class Endereco(BaseModel):
     estado: str
 
 # Classe representando os dados do cliente
+
+
 class Usuario(BaseModel):
     id: int
     nome: str
@@ -26,11 +30,15 @@ class Usuario(BaseModel):
     senha: str
 
 # Classe representando a lista de endereços de um cliente
+
+
 class ListaDeEnderecosDoUsuario(BaseModel):
     usuario: Usuario
     enderecos: List[Endereco] = []
 
 # Classe representando os dados do produto
+
+
 class Produto(BaseModel):
     id: int
     nome: str
@@ -38,6 +46,8 @@ class Produto(BaseModel):
     preco: float
 
 # Classe representando o carrinho de compras de um cliente com uma lista de produtos
+
+
 class CarrinhoDeCompras(BaseModel):
     id_usuario: int
     id_produtos: List[Produto] = []
@@ -51,3 +61,57 @@ db_end = {}        # todos os enderecos cadastrados
 db_end_usr = {}        # enderecos_dos_usuarios
 db_carrinhos = {}
 
+# Essa função cadastra um usuário no sistema do "e-commerce".
+
+
+@app.post("/usuario/")
+async def criar_usuário(usuario: Usuario):
+    if usuario.id in db_usuarios:
+        return FALHA
+    elif match(r"^[a-z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$", usuario.email) and match(r"^[a-z0-9.!#$%&'*+@\/=?^_`{|}~-]{3,}$", usuario.senha):
+        db_usuarios[usuario.id] = usuario
+        return OK
+    return FALHA
+
+# Essa função traz um usuário por vez de acordo com o id.
+
+
+@app.get("/usuario/{id}")
+async def retornar_usuario(id: int):
+    if id in db_usuarios:
+        return db_usuarios[id]
+    return FALHA
+
+# Essa função traz todos os usuários que foram inseridos posteriormente.
+
+
+@app.get("/usuarios")
+async def retorna_usuarios():
+    return db_usuarios
+
+# Essa função traz o usuário cuja o nome for igual ao solicitado.
+
+
+@app.get("/usuarios/nome")
+async def retornar_usuario_com_nome(nome: str):
+    for usuario in db_usuarios.values():
+        if usuario.nome == nome:
+            return usuario
+    return FALHA
+
+# Essa função serve para deletar o usuário.
+
+
+@app.delete("/usuario/{id}/")
+async def deletar_usuario(id: int):
+    if id in db_usuarios:
+        db_usuarios.pop(id)
+        return OK
+    return FALHA
+
+
+
+@app.get("/")
+async def bem_vinda():
+    site = "Seja bem vinda"
+    return site.replace('\n', '')
